70862e836b2fbbb5b1c64fa0618976090cfab73a
Ismael Gabri <ismaelgabri.developer@gmail.com>
2025-07-25 00:49:54 -0300

    Authentication sing hahed password

--- Callisto.Domain.Api/Controllers/UserController.cs
+++ Callisto.Domain.Api/Controllers/UserController.cs
@@ -1,12 +1,14 @@
 ﻿using Callisto.Domain.Commands;
 using Callisto.Domain.Commands.User_Commands.Input;
 using Callisto.Domain.Commands.User_Commands.Output;
+using Callisto.Domain.Entities;
 using Callisto.Domain.Handlers;
 using Callisto.Domain.Infra.Contexts;
 using Callisto.Domain.Infra.Repositories;
 using Callisto.Domain.Repositories;
 using Callisto.Domain.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Callisto.Domain.Api.Controllers
@@ -26,13 +28,21 @@ namespace Callisto.Domain.Api.Controllers
         [HttpPost]
         [Route("login")]
         [AllowAnonymous]
-        public async Task<ActionResult<dynamic>> Authenticate([FromServices] CallistoContext context, [FromBody] LoginCommand model)
+        public async Task<ActionResult<dynamic>> Authenticate(
+        [FromServices] CallistoContext context,
+        [FromBody] LoginCommand model)
         {
             var users = _userRepository.GetAllUsers();
-            var user = users.FirstOrDefault(x => x.Email.Address == model.Email && x.PasswordHash == model.Password);
 
+            var user = users.FirstOrDefault(x => x.Email.Address == model.Email);
             if (user == null)
                 return NotFound(new { message = "Usuário não encontrado" });
+
+            var hasher = new PasswordHasher<User>();
+            var result = hasher.VerifyHashedPassword(user, user.PasswordHash, model.Password);
+
+            if (result == PasswordVerificationResult.Failed)
+                return BadRequest(new { message = "Senha inválida" });
 
             var token = TokenService.Generate(user);
             return new
